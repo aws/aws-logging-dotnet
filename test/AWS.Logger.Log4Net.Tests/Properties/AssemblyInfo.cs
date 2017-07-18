fe8f10625037a8106e539591be847e027d43ef27
sattwik <spati2@ncsu.edu>
2017-07-18 14:09:08 -0700

    Sign the libraries and revert dependency library in Core so netcore1.0 is supported

--- test/AWS.Logger.Log4Net.Tests/Properties/AssemblyInfo.cs
+++ test/AWS.Logger.Log4Net.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AWS.Logger.Test45")]
+[assembly: AssemblyTitle("AWS.Logger.Log4Net.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AWS.Logger.Test45")]
+[assembly: AssemblyProduct("AWS.Logger.Log4Net.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
