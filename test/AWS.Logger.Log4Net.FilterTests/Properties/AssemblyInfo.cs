78b05f91e4d516c114e67f1a19bc7fc7fd581bae
Pati <sattwikp@amazon.com>
2017-12-04 16:53:17 -0800

    Adding assembly info and csproj changes for Log4net

--- test/AWS.Logger.Log4Net.FilterTests/Properties/AssemblyInfo.cs
+++ test/AWS.Logger.Log4Net.FilterTests/Properties/AssemblyInfo.cs
@@ -5,13 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("")]
+
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AWS.Logger.Log4Net.FilterTests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
