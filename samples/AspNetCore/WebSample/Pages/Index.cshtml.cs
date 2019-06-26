054e74c7760536d0c471c5841f567a622f885852
Norm Johanson <normj@amazon.com>
2019-06-25 22:23:35 -0700

    Update ASP.NET Core web sample to ASP.NET Core 2.1

---
+++ samples/AspNetCore/WebSample/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        ILogger<IndexModel> Logger { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            Logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Logger.LogInformation("Welcome to the AWS Logger. You are viewing the home page");
+        }
+    }
+}
--- samples/AspNetCore/WebSample/Startup.cs
+++ samples/AspNetCore/WebSample/Startup.cs
@@ -1,69 +1,67 @@
-﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Hosting;
-using System;
 
 namespace WebSample
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration, ILogger<Startup> logger)
         {
-            // Read the appsetting.json file for the configuration details
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
+            this._logger = logger;
         }
 
         public IConfiguration Configuration { get; }
 
+        private ILogger<Startup> _logger;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // Create a logging provider based on the configuration information passed through the appsettings.json
-            // You can even provide your custom formatting.
-            loggerFactory.AddAWSProvider(this.Configuration.GetAWSLoggingConfigSection(), 
-                formatter: (logLevel, message, exception) => $"[{DateTime.UtcNow}] {logLevel}: {message}");
-
-            // Create a logger instance from the loggerFactory
-            var logger = loggerFactory.CreateLogger<Program>();
-
             // Example Logging
-            logger.LogInformation("Check the AWS Console CloudWatch Logs console in us-east-1");
-            logger.LogInformation("to see messages in the log streams for the");
-            logger.LogInformation("log group AspNetCore.WebSample");
-
+            _logger.LogInformation("Check the AWS Console CloudWatch Logs console in us-east-1");
+            _logger.LogInformation("to see messages in the log streams for the");
+            _logger.LogInformation("log group AspNetCore.WebSample");
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
