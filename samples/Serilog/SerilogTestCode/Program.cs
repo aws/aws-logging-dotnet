5c461d75c3d1663c5e944e0fb00fc2ec1d4097ef
Sruti Guhathakurta <srutg@amazon.com>
2019-08-29 12:04:09 -0700

    added samples for Serilog with code config and file config

---
+++ samples/Serilog/SerilogTestCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Serilog;
+using AWS.Logger.SeriLog;
+using AWS.Logger;
+
+namespace SerilogTestCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AWSLoggerConfig configuration = new AWSLoggerConfig("Serilog.ConfigExample");
+            configuration.Region = "us-east-1";
+
+            var logger = new LoggerConfiguration()
+            .WriteTo.AWSSeriLog(configuration)
+            .CreateLogger();
+
+            logger.Information("Hello!");
+        }
+    }
+}
---
+++ samples/Serilog/SerilogTestCodeFromConfig/Program.cs
@@ -0,0 +1,22 @@
+﻿using Serilog;
+using Microsoft.Extensions.Configuration;
+
+namespace SerilogTestCodeFromConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // logger configuration reads from appsettings.json
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            var logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+            logger.Information("Hello!");
+        }
+    }
+}
