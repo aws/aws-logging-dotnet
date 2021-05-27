cda54a0cacb9d5a714ce0fc2f563d32a67c1d086
ashishdhingra <67916761+ashishdhingra@users.noreply.github.com>
2021-05-26 17:05:31 -0700

    Fixed an issue where restrictedToMinimumLevel from appsettings.json was ignored.

---
+++ samples/Serilog/SerilogTestCodeFromConfigRestrictedToMinimumLevel/Program.cs
@@ -0,0 +1,23 @@
+﻿using Serilog;
+using Microsoft.Extensions.Configuration;
+
+namespace SerilogTestCodeFromConfigRestrictedToMinimumLevel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // logger configuration reads from appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            logger.Information("This should not log since restrictedToMinimumLevel is Error in appsettings.json!");
+            logger.Error("Hello!");
+        }
+    }
+}
